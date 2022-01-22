1419d41b99e5ba275d7db9bb647f59acf737e781
aminul-official <office.aminulislam565688@gmail.com>
2022-01-22 12:49:48 +0600

    side bar is done for laptop and desktop

--- setting.js
+++ setting.js
@@ -1,1 +1,10 @@
-console.log('all ok');
+// Add active class to the current button (highlight it)
+var navMain = document.getElementById("navMain");
+var navItem = navMain.getElementsByClassName("nav-item");
+for (var i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active-nav");
+  current[0].className = current[0].className.replace(" active-nav", "");
+  this.className += " active-nav";
+  });
+}
