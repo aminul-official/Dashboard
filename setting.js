50aa17ddb5f9763214f829a4f9e4bffd8a83a01d
aminul-official <office.aminulislam565688@gmail.com>
2022-01-22 16:45:09 +0600

    sidebar is almost done transition is remaining

--- setting.js
+++ setting.js
@@ -8,3 +8,24 @@ for (var i = 0; i < navItem.length; i++) {
   this.className += " active-nav";
   });
 }
+
+
+// Open menu
+
+function openMenu() {
+    var x = document.getElementById("navMain");
+    console.log('this is from sibilings');
+  }
+
+
+// mobile menu
+
+function mobileMenu() {
+    const mobileMenuIcon = document.getElementById('mobileMenuIcon');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.style.width = '44px';
+    mobileMenuIcon.classList.toggle("mobile-menu-icon");
+    console.log('working');
+    var mobileNavMainx = document.getElementById("mobileNavMain");
+    mobileNavMainx.classList.toggle('mobileNavMain-open');
+}
